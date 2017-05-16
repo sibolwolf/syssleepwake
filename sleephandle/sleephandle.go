824617f2c8eb923123bd453656fe63f2c2293281
sibolwolf <hejinlai@foxmail.com>
2017-05-16 18:19:04 +0800

    update sleep and wake module
    Change-Id: I37a2f920d4ad1e1645d64e6c1372bfb0c8cdc410

--- sleephandle/sleephandle.go
+++ sleephandle/sleephandle.go
@@ -6,45 +6,40 @@ import (
 )
 
 func SleepHandle() {
-    log.Println("SleepHandle: Close Wifi")
+    log.Println("#1. SleepHandle: Close Wifi ...")
     cmd_wifi_down := exec.Command("/bin/sh", "-c", "ifconfig wlan0 down")
     cmd_wifi_down_output, cmd_wifi_down_err := cmd_wifi_down.Output()
     if cmd_wifi_down_err != nil {
         log.Println("cmd_wifi_down_err: " + cmd_wifi_down_err.Error())
-        return
     }
     log.Println(string(cmd_wifi_down_output))
 
-    log.Println("SleepHandle: Stop RA")
+    log.Println("#2. SleepHandle: Stop RA ...")
     cmd_ra_down := exec.Command("/bin/sh", "-c", "/etc/init.d/ra stop")
     _, cmd_ra_down_err := cmd_ra_down.Output()
     if cmd_ra_down_err != nil {
         log.Println("cmd_ra_down_err: " + cmd_ra_down_err.Error())
-        return
     }
 
-    log.Println("SleepHandle: Echo test > wake_lock")
+    log.Println("#3. SleepHandle: Echo test > wake_lock ...")
     cmd_wake_lock := exec.Command("/bin/sh", "-c", "echo test > /sys/power/wake_lock")
     _, cmd_wake_lock_err := cmd_wake_lock.Output()
     if cmd_wake_lock_err != nil {
         log.Println("cmd_wake_lock_err: " + cmd_wake_lock_err.Error())
-        return
     }
 
-    log.Println("SleepHandle: Echo mem > power/state")
+    log.Println("#4. SleepHandle: Echo mem > power/state ...")
     cmd_mem_down := exec.Command("/bin/sh", "-c", "echo mem > /sys/power/state")
     _, cmd_mem_down_err := cmd_mem_down.Output()
     if cmd_mem_down_err != nil {
         log.Println("cmd_mem_down_err: " + cmd_mem_down_err.Error())
-        return
     }
 
-    log.Println("SleepHandle: Echo mem > wake_unlock")
+    log.Println("#5. SleepHandle: Echo mem > wake_unlock ...")
     cmd_wake_unlock := exec.Command("/bin/sh", "-c", "echo test > /sys/power/wake_unlock")
     _, cmd_wake_unlock_err := cmd_wake_unlock.Output()
     if cmd_wake_unlock_err != nil {
         log.Println("cmd_wake_unlock_err: " + cmd_wake_unlock_err.Error())
-        return
     }
 
 }
