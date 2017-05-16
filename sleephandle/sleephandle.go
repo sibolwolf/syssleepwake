b14b107140a51748cfa8a064d107877c6bec1531
sibolwolf <hejinlai@foxmail.com>
2017-05-16 17:27:40 +0800

    update sleephandle/wakehandle
    Change-Id: I316b383325a09638ea82000902bf10c744111d59

--- sleephandle/sleephandle.go
+++ sleephandle/sleephandle.go
@@ -7,8 +7,8 @@ import (
 
 func SleepHandle() {
     cmd_wifi_down := exec.Command("/bin/sh", "-c", "ifconfig wlan0 down")
-    cmd_wifi_down_output, cmd_wifi_downerr := cmd_wifi_down.Output()
-    if err != nil {
+    cmd_wifi_down_output, cmd_wifi_down_err := cmd_wifi_down.Output()
+    if cmd_wifi_down_err != nil {
         log.Println("cmd_wifi_down_err: " + cmd_wifi_down_err.Error())
         return
     }
@@ -16,28 +16,28 @@ func SleepHandle() {
 
     cmd_ra_down := exec.Command("/bin/sh", "-c", "/etc/init.d/ra stop")
     _, cmd_ra_down_err := cmd_ra_down.Output()
-    if err != nil {
+    if cmd_ra_down_err != nil {
         log.Println("cmd_ra_down_err: " + cmd_ra_down_err.Error())
         return
     }
 
     cmd_wake_lock := exec.Command("/bin/sh", "-c", "echo test > /sys/power/wake_lock")
     _, cmd_wake_lock_err := cmd_wake_lock.Output()
-    if err != nil {
+    if cmd_wake_lock_err != nil {
         log.Println("cmd_wake_lock_err: " + cmd_wake_lock_err.Error())
         return
     }
 
     cmd_mem_down := exec.Command("/bin/sh", "-c", "echo mem > /sys/power/state")
     _, cmd_mem_down_err = cmd_mem_down.Output()
-    if err != nil {
+    if cmd_mem_down_err != nil {
         log.Println("cmd_mem_down_err: " + cmd_mem_down_err.Error())
         return
     }
 
     cmd_wake_unlock := exec.Command("/bin/sh", "-c", "echo test > /sys/power/wake_unlock")
     _, cmd_wake_unlock_err = cmd_wake_unlock.Output()
-    if err != nil {
+    if cmd_wake_unlock_err != nil {
         log.Println("cmd_wake_unlock_err: " + cmd_wake_unlock_err.Error())
         return
     }
--- wakehandle/wakehandle.go
+++ wakehandle/wakehandle.go
@@ -8,14 +8,14 @@ import (
 func WakeHandle() {
     cmd_wake_lock := exec.Command("/bin/sh", "-c", "echo test > /sys/power/wake_lock")
     _, cmd_wake_lock_err := cmd_wake_lock.Output()
-    if err != nil {
+    if cmd_wake_lock_err != nil {
         log.Println("cmd_wake_lock_err: " + cmd_wake_lock_err.Error())
         return
     }
 
     cmd_wifi_up := exec.Command("/bin/sh", "-c", "ifconfig wlan0 up")
-    cmd_wifi_up_output, cmd_wifi_up_err := cmd_wake_lock.Output()
-    if err != nil {
+    cmd_wifi_up_output, cmd_wifi_up_err := ccmd_wifi_up.Output()
+    if cmd_wifi_up_err != nil {
         log.Println("cmd_wifi_up_err: " + cmd_wifi_up_err.Error())
         return
     }
@@ -23,7 +23,7 @@ func WakeHandle() {
 
     cmd_ra_up := exec.Command("/bin/sh", "-c", "/etc/init.d/ra start")
     cmd_ra_up_output, cmd_ra_up_err := cmd_ra_up.Output()
-    if err != nil {
+    if cmd_ra_up_err != nil {
         log.Println("cmd_ra_up_err: " + cmd_ra_up_err.Error())
         return
     }
