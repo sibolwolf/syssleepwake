758911893bee833c4b517506481b41f121a59a2c
sibolwolf <hejinlai@foxmail.com>
2017-05-17 10:24:30 +0800

    change wakehandle
    Change-Id: Iaa36f4f3c9e0ac59da65c2c3fb7502079f21e859

--- wakehandle/wakehandle.go
+++ wakehandle/wakehandle.go
@@ -11,23 +11,25 @@ func WakeHandle() {
     KEY.GetButton("power").OnPress(func() {
             log.Println("RA got a short key press event for power")
             WakeHandleAction()
+            return
         })
 
     KEY.GetButton("home").OnPress(func() {
             log.Println("RA got a short key press event for home")
             WakeHandleAction()
+            return
         })
 }
 
 func WakeHandleAction() {
-    log.Println("#1. SleepHandle: Echo test > wake_lock ..........")
+    log.Println("#1. WakeHandle: Echo test > wake_lock ..........")
     cmd_wake_lock := exec.Command("/bin/sh", "-c", "echo test > /sys/power/wake_lock")
     _, cmd_wake_lock_err := cmd_wake_lock.Output()
     if cmd_wake_lock_err != nil {
         log.Println("cmd_wake_lock_err: " + cmd_wake_lock_err.Error())
     }
 
-    log.Println("#2. SleepHandle: Open Wifi ..........")
+    log.Println("#2. WakeHandle: Open Wifi ..........")
     cmd_wifi_up := exec.Command("/bin/sh", "-c", "ifconfig wlan0 up")
     _, cmd_wifi_up_err := cmd_wifi_up.Output()
     if cmd_wifi_up_err != nil {
@@ -35,7 +37,7 @@ func WakeHandleAction() {
     }
     //log.Println(string(cmd_wifi_up_output))
 
-    log.Println("#3. SleepHandle: Start RA ..........")
+    log.Println("#3. WakeHandle: Start RA ..........")
     cmd_ra_up := exec.Command("/bin/sh", "-c", "/etc/init.d/ra start")
     _, cmd_ra_up_err := cmd_ra_up.Output()
     if cmd_ra_up_err != nil {
